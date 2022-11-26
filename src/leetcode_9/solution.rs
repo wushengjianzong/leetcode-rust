ff1bc90f8c912bbb50d53d17cbba593334de0375
wushengjianzong <943459049@qq.com>
2022-11-27 00:12:51 +0800

    submit 9

--- src/leetcode_9/solution.rs
+++ src/leetcode_9/solution.rs
@@ -7,19 +7,19 @@ struct Solution;
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         let mut x: i32 = x;
-        if x < 0 {
+        if x < 10 {
+            return x >= 0;
+        }
+        if x % 10 == 0 {
             return false;
         }
-        if x < 10 {
-            return true;
-        }
         let mut y: i32 = 0;
-        while x > (y / 10) {
+        while x > y {
             y = y * 10 + x % 10;
             x = x / 10;
             println!("x={} y={}", x, y);
         }
-        return (x == y) | (x == (y / 10));
+        return (x == y) | (x == y / 10);
     }
 }
 
@@ -31,5 +31,7 @@ mod tests {
     fn test_solution() {
         println!("{}", Solution::is_palindrome(121));
         println!("{}", Solution::is_palindrome(10));
+        println!("{}", Solution::is_palindrome(99));
+        println!("{}", Solution::is_palindrome(1221)); 
     }
 }
